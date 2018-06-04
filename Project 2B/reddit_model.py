ae466b1a1a4fb2adb0b00d5de5902fa8a16ad431
Arpit Jasapara <jasapara.arpit@gmail.com>
2018-06-03 18:21:05 -0700

    changed code structure

--- Project 2B/reddit_model.py
+++ Project 2B/reddit_model.py
@@ -8,6 +8,9 @@ def main(context):
     """Main function takes a Spark SQL context."""
     # YOUR CODE HERE
     # YOU MAY ADD OTHER FUNCTIONS AS NEEDED
+    comments = context.read.json("comments-minimal.json.bz2")
+	submissions = context.read.json("submissions.json.bz2")
+	labels = context.read.format('csv').options(header='true', inferSchema='true').load("labeled_data.csv")
 
 if __name__ == "__main__":
     conf = SparkConf().setAppName("CS143 Project 2B")
@@ -15,8 +18,5 @@ if __name__ == "__main__":
     sc   = SparkContext(conf=conf)
     sqlContext = SQLContext(sc)
     sc.addPyFile("cleantext.py")
-    comments = sqlContext.read.json("comments-minimal.json.bz2")
-	submissions = sqlContext.read.json("submissions.json.bz2")
-	labels = sqlContext.read.format('csv').options(header='true', inferSchema='true').load("labeled_data.csv")
     main(sqlContext)
 
