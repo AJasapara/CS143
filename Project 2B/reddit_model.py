4bdf8466123ea305fc1eeb4d63a6435359f1781a
Arpit Jasapara <jasapara.arpit@gmail.com>
2018-06-03 20:30:25 -0700

    updated reddit-model

--- Project 2B/reddit_model.py
+++ Project 2B/reddit_model.py
@@ -8,9 +8,21 @@ def main(context):
     """Main function takes a Spark SQL context."""
     # YOUR CODE HERE
     # YOU MAY ADD OTHER FUNCTIONS AS NEEDED
-    comments = context.read.parquet("comments.parquet")
-    submissions = context.read.parquet("submissions.parquet")
-    labels = context.read.parquet("labels.parquet")
+    try:
+        comments = context.read.parquet("comments.parquet")
+    except:
+    	comments = context.read.json("comments-minimal.json.bz2")
+    	comments.write.parquet("comments.parquet")
+    try:
+        submissions = context.read.parquet("submissions.parquet")
+    except:
+    	submissions = context.read.json("submissions.json.bz2")
+    	submissions.write.parquet("submissions.parquet")
+    try:
+        labels = context.read.parquet("labels.parquet")
+    except:
+    	labels = context.read.format('csv').options(header='true', inferSchema='true').load("labeled_data.csv")
+    	labels.write.parquet("labels.parquet")
 
 if __name__ == "__main__":
     conf = SparkConf().setAppName("CS143 Project 2B")
