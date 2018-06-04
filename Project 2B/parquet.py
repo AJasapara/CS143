a191930f9f6df08754bf1041f588aa89476a1867
Arpit Jasapara <jasapara.arpit@gmail.com>
2018-06-03 18:45:53 -0700

    added parquet

--- Project 2B/parquet.py
+++ Project 2B/parquet.py
@@ -8,9 +8,12 @@ def main(context):
     """Main function takes a Spark SQL context."""
     # YOUR CODE HERE
     # YOU MAY ADD OTHER FUNCTIONS AS NEEDED
-    comments = context.read.parquet("comments.parquet")
-    submissions = context.read.parquet("submissions.parquet")
-    labels = context.read.parquet("labels.parquet")
+    comments = context.read.json("comments-minimal.json.bz2")
+    submissions = context.read.json("submissions.json.bz2")
+    labels = context.read.format('csv').options(header='true', inferSchema='true').load("labeled_data.csv")
+    comments.write.parquet("comments.parquet")
+    submissions.write.parquet("submissions.parquet")
+    labels.write.parquet("labels.parquet")
 
 if __name__ == "__main__":
     conf = SparkConf().setAppName("CS143 Project 2B")
